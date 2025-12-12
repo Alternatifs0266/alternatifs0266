774cca2af31904f8030571d92aaee2fca4e0fe6f
Alternatifs0266 <alternatifs0266@free.fr>
2025-12-12 22:45:15 +0100

    Add script for launch all launch_all.sh

---
+++ scripts/analyse_adif/launch_all.sh
@@ -0,0 +1,7 @@
+python3 analyze_adif_dx_performance.py
+python3 analyze_adif_schedule.py
+python3 analyze_snr_performance.py
+python3 find_top_dx_contacts.py
+python3 analyze_adif_mode.py
+~/.venv/bin/python analyze_greyline_dx.py
+python3 analyze_weekly_traffic.py
