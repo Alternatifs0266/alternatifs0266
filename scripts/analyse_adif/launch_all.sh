936a24ef16f2f578fb9bb5a19b02292f4ad1cc94
f4exb <f4exb06@gmail.com>
2025-12-14 01:37:20 +0100

    Fixed launch_all.sh

--- scripts/analyse_adif/launch_all.sh
+++ scripts/analyse_adif/launch_all.sh
@@ -1,4 +1,7 @@
 if [ -n ".venv" ]; then
+    echo "Using existing virtual environment."
+else
+    echo "Creating virtual environment..."
     python3 -m venv .venv
     source .venv/bin/activate
     pip install -r requirements.txt
